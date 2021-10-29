b58e6b2a4f746275a3d47ad63341bd7bab14e138
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2021-10-29 01:12:44 -0300

    search result cards

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,37 +1,43 @@
 import React, { useEffect, useMemo, useState, useRef } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { TextInput, Button, RadioButton } from 'react-native-paper';
+import { View, StyleSheet, ScrollView, TouchableWithoutFeedback, ImageBackground } from 'react-native';
+import { TextInput, Button, RadioButton, Text } from 'react-native-paper';
+import { Item } from 'react-native-paper/lib/typescript/components/List/List';
 import { useSelector } from 'react-redux';
 import { fetchMediaWithTitle } from '../api/anilist';
-import { MediaType } from '../model/anilist';
+import { MediaType, Media } from '../model/anilist';
 import { StoreState } from '../store/store';
 
 export default function SearchScreen() {
     const anilist = useSelector((state: StoreState) => state.anilist); 
     const [mediaType, setMediaType] = useState<MediaType>(MediaType.ANIME);
+    const [result, setResult] = useState<Media[]>([]);
+    const [searchQuery, setSearchQuery] = useState('');
     
     const handleSearch = async () => {
-        const title = 'naruto';
+        if (searchQuery.trim().length === 0) {
+            // do nothing if the query is empty
+            setResult([]);
+            return;
+        }
 
         try {
-            const medias = await fetchMediaWithTitle(title, mediaType);
-            for (const media of medias) {
-                console.log(media.title.romaji);
-            }
+            const medias = await fetchMediaWithTitle(searchQuery, mediaType);
+            setResult(medias);
         } catch(e) {
             console.error(e);
         }
     };
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <View style={styles.form}>
-                
                 <View style={{ alignItems: 'center' }}>
                     <View style={{ width: 300, height: 120, top: 30 }}>
                         <TextInput
                             label="Title"
                             style={styles.textInput}
+                            value={searchQuery}
+                            onChangeText={value => setSearchQuery(value)}
                         />
                     </View>
                     
@@ -54,7 +60,36 @@ export default function SearchScreen() {
                     </Button>
                 </View>
             </View>
-        </View>
+
+            <View style={styles.resultsContainer}>
+                {(result.length > 0) && (
+                    <Text style={{ fontSize: 19 }}>
+                        Results: 
+                    </Text>
+                )}
+
+                {result.map(media => (
+                    <View 
+                        key={media.id}
+                        style={styles.itemCard}
+                    >
+                        <TouchableWithoutFeedback 
+                            style={styles.cardCoverImage}
+                            //onPress={openMediaPage}
+                        >
+                            <ImageBackground
+                                style={styles.cardCoverImage} 
+                                source={{ uri: media.coverImage.large }} 
+                            />
+                        </TouchableWithoutFeedback>
+
+                        <Text style={styles.text} numberOfLines={1}>
+                            {media.title.romaji}
+                        </Text>
+                    </View>
+                ))}
+            </View>
+        </ScrollView>
     );
 }
 
@@ -68,13 +103,29 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     textInput: {
-        //flex: 1,
-        //marginBottom: 10,
-        backgroundColor: '#174a97',
-        //height: 100,
-        //maxWidth: 250,
+        backgroundColor: '#174a97',    
     },
     searchButton: {
         width: 200,
+    },
+    itemCard: {
+        backgroundColor: '#151F2E',
+        height: 115,
+        width: 305,
+        borderRadius: 3,
+        flexDirection: 'row',
+        margin: 5,
+    },
+    cardCoverImage: {
+        height: 115,
+        width: 85,
+    },
+    resultsContainer: {
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 16,
+        margin: 5,
+        maxWidth: 210,
     },
 });
