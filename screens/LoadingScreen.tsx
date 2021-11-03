75013fe137e52a41a9035a1c39b2d8b38e8d9d16
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2021-11-02 21:29:08 -0300

    login button

--- screens/LoadingScreen.tsx
+++ screens/LoadingScreen.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Linking } from 'react-native';
 import ExpoLinking from 'expo-linking';
+import Constants from 'expo-constants';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/core';
 import { fetchViewer } from '../api/anilist';
-import { ActivityIndicator, Colors } from 'react-native-paper';
+import { ActivityIndicator, Colors, Button } from 'react-native-paper';
 import { anilistSlice, StoreState } from '../store/store';
+
+const authUrl = `https://anilist.co/api/v2/oauth/authorize`;
+const clientId: string = Constants.manifest?.extra?.anilistClientId;
 
 export default function LoadingScreen() {
     const dispatch = useDispatch();
@@ -15,11 +20,16 @@ export default function LoadingScreen() {
 
     const [isLoading, setIsLoading] = useState(false);
     const [isLoggedIn, setisLoggedIn] = useState(false);
-    const [wasOpenViaURL, setWasOpenViaURL] = useState(false);
 
     const openHomeScreen = () => {
         // @ts-ignore
         navigation.navigate('Main');
+    };
+
+    const logOut = async () => {
+        await AsyncStorage.removeItem('anilist_token');
+        dispatch(anilistSlice.actions.setUser(undefined));
+        dispatch(anilistSlice.actions.setToken(undefined));
     };
 
     const fetchData = async (accessToken: string) => {
@@ -28,12 +38,15 @@ export default function LoadingScreen() {
         dispatch(anilistSlice.actions.setToken(accessToken));
     };
 
+    const handleLogInBtnPress = () => {
+        Linking.openURL(authUrl + '?client_id=' + clientId + '&response_type=token');
+    };
+
     useEffect(() => {
         Linking.addEventListener('url', async (e) => {
-            setWasOpenViaURL(true);
-
             if (state.anilist.token == null) {
                 try {
+                    setIsLoading(true);
                     const parsedURL = ExpoLinking.parse(e.url.replace('#', '?'));
                     const accessToken = parsedURL.queryParams['access_token']
                     if (typeof accessToken !== 'string') {
@@ -45,6 +58,7 @@ export default function LoadingScreen() {
                     setisLoggedIn(true);
                     openHomeScreen();
                 } catch (err) {
+                    await logOut();
                     console.error(err);
                 }
 
@@ -59,16 +73,33 @@ export default function LoadingScreen() {
                     setisLoggedIn(true);
                     openHomeScreen();
                 } catch (err) {
+                    await logOut();
                     setisLoggedIn(false);
                     console.error(err);
                 }
             } else {
+                await logOut();
                 setisLoggedIn(false);
             }
 
             setIsLoading(false);
         });
     }, []);
+
+    if (!isLoading && !isLoggedIn) {
+        return (
+            <View style={styles.container}>
+                <Button 
+                    mode="contained"
+                    onPress={handleLogInBtnPress}
+                    color="#174a97"
+                >
+                    Log in
+                </Button>
+            </View>
+        );
+    }
+
 
     return (
         <View style={styles.container}>
